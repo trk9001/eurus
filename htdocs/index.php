52567cd83aa150dd4ca6bb632ee0860bcc3606f3
trk9001 <dev.trk.9001@gmail.com>
2020-09-13 03:29:11 +0600

    Add a temporary landing page

---
+++ htdocs/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Eurus</title>
+
+<h1>Eurus</h1>
+<hr>
+
+<p><em>There's an east wind coming, Watson.</em></p>
