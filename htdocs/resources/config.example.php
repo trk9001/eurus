9de5eb91f07d13ab58214e8148b63b9ebeb3bb0b
trk9001 <dev.trk.9001@gmail.com>
2020-09-27 23:29:19 +0600

    Add a definition for PUBLIC_SCRIPTS_DIR

--- htdocs/resources/config.example.php
+++ htdocs/resources/config.example.php
@@ -13,6 +13,13 @@ if (!defined('LIBRARY_DIR')) {
     define('LIBRARY_DIR', realpath(__DIR__ . '/library'));
 }
 
+if (!defined('PUBLIC_SCRIPTS_DIR')) {
+    $prefix = dirname(__DIR__);
+    $pubscr_path = substr(__DIR__, strlen($prefix));
+    // The `realpath` function doesn't seem to work for the next line.
+    define('PUBLIC_SCRIPTS_DIR', $pubscr_path . '/library/scripts');
+}
+
 if (!defined('TEMPLATES_DIR')) {
     define('TEMPLATES_DIR', realpath(__DIR__ . '/templates'));
 }
